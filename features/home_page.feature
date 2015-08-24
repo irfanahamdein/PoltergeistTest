fb0133700a06731cee8d8550e11b6ab40995df1d
irfanah <irfan.ahmad@tinyowl.co.in>
2015-08-24 10:57:07 +0530

    my changes

--- features/home_page.feature
+++ features/home_page.feature
@@ -2,12 +2,12 @@
 Feature: Google Search to explore poltergiest
   In order to expolre poltergeist functionality
   As a capybara and poltergeist user
-  I want to see the if it works on Google search page 
+  I want to see the if it works on Google search page
 
 
 Scenario: View home page
   Given I am on the home page
-  When I fill in "q" with "shashi"
+  When I fill in "q" with "irfan"
   Then I should see "Sign in"
  
  		
--- features/support/env.rb
+++ features/support/env.rb
@@ -2,7 +2,10 @@ require "capybara/cucumber"
 require "rspec"
 require 'capybara/poltergeist'
 
-Capybara.default_driver = :poltergeist
+#Capybara.register_driver :chrome do |app|
+#  Capybara::Selenium::Driver.new(app, :browser => :chrome)
+#end
+
 Capybara.register_driver :poltergeist do |app|
 
     options = {
@@ -11,8 +14,9 @@ Capybara.register_driver :poltergeist do |app|
         :debug => false,
         :phantomjs_options => ['--load-images=no', '--disk-cache=false'],
         :inspector => true,
-
     }
 
-    Capybara::Poltergeist::Driver.new(app, options)
+   Capybara::Poltergeist::Driver.new(app, options)
 end
+
+Capybara.default_driver = :poltergeist
